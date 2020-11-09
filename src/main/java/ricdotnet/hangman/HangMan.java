dccb6c9199c40419f4f940442a027fe68020fa23
Ricardo Rocha <me@rrocha.uk>
2020-11-09 22:43:19 +0000

    snapshot 2.0

--- src/main/java/ricdotnet/hangman/HangMan.java
+++ src/main/java/ricdotnet/hangman/HangMan.java
@@ -15,7 +15,7 @@ public class HangMan {
     static Scanner input = new Scanner(System.in); //scanner for user inputs
     static Random rand = new Random(); //random number generator
     
-    static String[] words = {"banana", "theory", "speaker", "laptop", "keyboard"}; //array list for words
+    static String[] words = {"banana", "ananas", "adriana", "hippopotamus", "national"}; //array list for words
     
     //create variables
     static boolean finished = false;
@@ -25,6 +25,13 @@ public class HangMan {
     static int correctGuesses = 0;
     static int selwordchar = 0;
     
+    static char[][] hangman = {{'-','-','-','-','-'},
+            {'|',' ',' ',' ','|'},
+            {'|',' ',' ',' ','|'},
+            {'|',' ',' ',' ','|'},
+            {'|',' ',' ',' ','|'},
+            {'-','-','-','-','-'}};
+    
     static int line;
     static int column;
     
@@ -32,25 +39,90 @@ public class HangMan {
     public static void getWord(){
         int number = rand.nextInt(4) + 1;
         word = words[number];
+        //String str1 = new String(word);
     }
     
     //method for the hangman
-    public static void hangMan(int line, int column){
-        char[][] hangman = {{' ',' ',' '},
-            {' ',' ',' '},
-            {' ',' ',' '},
-            {' ',' ',' '}};
-        for(char[] row : hangman){
-            for(char c:row){
-                System.out.print(c);
+    public static void hangMan(int wrongGuess){
+        
+        if(wrongGuess == 1){
+            hangman[1][2] = 'O';
+            for(char[] row : hangman){
+                for(char line : row){
+                    System.out.print(line);
+                }
+                System.out.println();
             }
-            System.out.println();
+        }else if(wrongGuesses == 2){
+            hangman[2][2] = '|';
+            for(char[] row : hangman){
+                for(char line : row){
+                    System.out.print(line);
+                }
+                System.out.println();
+            }
+        }else if(wrongGuesses == 3){
+            hangman[3][2] = '|';
+            for(char[] row : hangman){
+                for(char line : row){
+                    System.out.print(line);
+                }
+                System.out.println();
+            }
+        }else if(wrongGuesses == 4){
+            hangman[4][1] = '/';
+            for(char[] row : hangman){
+                for(char line : row){
+                    System.out.print(line);
+                }
+                System.out.println();
+            }
+        }else if(wrongGuesses == 5){
+            hangman[4][3] = '\\';
+            for(char[] row : hangman){
+                for(char line : row){
+                    System.out.print(line);
+                }
+                System.out.println();
+            }
+        }else if(wrongGuesses == 6){
+            hangman[2][1] = '/';
+            for(char[] row : hangman){
+                for(char line : row){
+                    System.out.print(line);
+                }
+                System.out.println();
+            }
+        }else if(wrongGuesses == 7){
+            hangman[2][3] = '\\';
+            for(char[] row : hangman){
+                for(char line : row){
+                    System.out.print(line);
+                }
+                System.out.println();
+            }
+        }else{
+            for(char[] row : hangman){
+                for(char line:row){
+                    System.out.print(line);
+                }
+                System.out.println();
+            }
         }
     }
     
-    public static void repeatingChars(int x){
+    /*public static void repeatingChars(int x){
         for(int i = 0; i < word.length(); i++){
             
+        }
+    }*/
+    
+    public static void checkWinner(int correctGuesses){
+        //if statement to check if the game ended or not yet
+        if(correctGuesses == word.length()-1){
+            System.out.println("Game finished. You guessed the word. :)\n"
+                + "It was " + word + ".\n");
+                finished = true;
         }
     }
     
@@ -60,28 +132,18 @@ public class HangMan {
         getWord();
         System.out.println(word);
         
-        /*for(int i = 0; i < word.length(); i++){
-            for(int j = 0; j < word.length(); j++){
-                if(word[i] == word[j]){
-                    
-                }
-            }
-        }*/
-        
         String selword[] = new String[word.length()]; //create an array with the length of the word
         Arrays.fill(selword, "*");
         
+        //show the user underscore for each character
         for(int i = 0; i < word.length(); i++){
             System.out.print(selword[i]);
         }
         System.out.println("\n"); //print next line
         
-        //show the user underscore for each character
-        //for(int i = 0; i < word.length(); i++){
-        //    System.out.print(selword[i]);
-        //}
-        //System.out.println("\n"); //print next line
+        hangMan(wrongGuesses); //show the hangman space
         
+        //give a hint to the user
         System.out.println("The word contains " + word.length() + " characters. \n"
                 + "You have 7 tries to guess the word.\n");
         
@@ -93,50 +155,57 @@ public class HangMan {
             
             //System.out.println(wrongGuesses);
             if(!word.contains(character)){
+                wrongGuesses += 1; //add a wrong try to the counter
                 System.out.println("The word does not contain the character '" + character + "'.\n"
-                        + "Please enter a new character: ");
-                character = input.next();
-                wrongGuesses += 1; //add a try to the counter
+                        + "A piece was added to your hanging man.\n");
                 
+                hangMan(wrongGuesses); //show the hanging man
                 //if statement to check if the game ended or not yet
                 if(wrongGuesses == 7){
                     System.out.println("Game finished. You lost. :(");
                     finished = true;
+                }else{
+                    //if there are not 7 wrong guesses ask again
+                    System.out.println("Please enter a new character: ");
+                    character = input.next();
                 }
+                
             }else{
+                //System.out.println("You have " + correctGuesses);
                 if(Arrays.asList(selword).contains(character)){
                     System.out.println("You already entered this. \n"
                             + "Enter a new character: ");
                     character = input.next();
                 }else{
-                    selword[selwordchar] = character; //add valid character to the list
+                    //selword[selwordchar] = character; //add valid character to the list
                 
+                    //for loop to replace each * with the chosen character
+                    for(int i = 0; i < word.length(); i++){
+                        if(character.equals(String.valueOf(word.charAt(i)))){
+                            selword[i] = character;
+                            correctGuesses += 1;
+                            //replace the space and add 1 correct guess
+                        }
+                        //System.out.println(word.charAt(i));
+                    }
+                    System.out.println();
+                    
+                    //System.out.println("Index of " + character + " in word: "+word.indexOf(character));
+                    
+                    //pring the word with the hints
                     for(int i = 0; i < word.length(); i++){
                         System.out.print(selword[i]);
                     }
-                    System.out.println("");
+                    System.out.println();
                 
-                    System.out.println("Nice, there is a " + character + " in the word.\n"
-                            + "Enter a new character: ");
+                    System.out.println("Nice, the word contains one or more '" + character + "'.\n");
+                    System.out.printf("\nYou have now %d correct characters.\n\n", correctGuesses); //show the user how many correct guesses they have
+                    
+                    System.out.println("Enter a new character: ");
                     character = input.next();
-                    correctGuesses += 1; //add 1 correct guess
-                    selwordchar += 1; //select the next character on the hidden word
-                
-                
-                //replace underscore with the character
-                //if(character.equals(word.charAt(selwordchar))){
-                //    selword[selwordchar] = character;
-                //    for(int i = 0; i < word.length(); i++){
-                //        System.out.print(selword[i]);
-                //    }
-                //    System.out.println();
-                //}
-                
-                    if(correctGuesses == word.length()){
-                        System.out.println("Game finished. You guessed the word. :)\n"
-                                + "It was " + word + ".");
-                        finished = true;
-                    }
+                    //correctGuesses += 1; //add 1 correct guess
+                    //selwordchar += 1; //select the next character on the hidden word
+                    checkWinner(correctGuesses);
                 }
             }
         }//end while loop
